db4e6e9a1cbcfd7cd47aa10de20519360748c198
Radixos <radixomen@gmail.com>
2022-09-20 10:35:26 +0100

    * Created member interface * Added member service * Added a posibility to retrieve a list of members * Created member cards * Added styling * Added animation to buttons * Using interceptor to send tokens * Routing the detailed page * Added styling to member detailed page

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -13,11 +13,12 @@ export class MemberListComponent implements OnInit {
   constructor(private memberService: MembersService) { }
 
   ngOnInit(): void {
+    this.loadMembers();
   }
 
   loadMembers() {
     this.memberService.getMembers().subscribe(members => {
-      this.members = this.members;
+      this.members = members;
     })
   }
 

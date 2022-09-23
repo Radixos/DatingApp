d95b5d0283dab584b4aa66b18a2a1131803ac34b
Radixos <radixomen@gmail.com>
2022-09-23 17:31:50 +0100

    * Added likes entity, repository * Implemented likes repository * Added controller methods for the likes feature * Set up likes functions in Angular * Added the likes component * Added pagination for likes

--- client/src/app/_interceptors/error.interceptor.ts
+++ client/src/app/_interceptors/error.interceptor.ts
@@ -29,12 +29,16 @@ export class ErrorInterceptor implements HttpInterceptor {
                   }
                 }
                 throw modalStateErrors.flat();
-              } else {
+              } else if (typeof(error.error) === "object") {
+                //this.toastr.error(error.statusText, error.status);
                 this.toastr.error(error.statusText === "OK" ? "Bad Request" : error.statusText, error.status);
+              } else {
+                this.toastr.error(error.error, error.status);
               }
               break;
 
             case 401:
+              //this.toastr.error(error.statusText, error.status);
               this.toastr.error(error.statusText === "Something unexpected went wrong" ? "Unauthorised" : error.statusText, error.status);
               break;
 

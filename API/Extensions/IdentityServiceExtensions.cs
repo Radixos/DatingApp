7cdf342dcd540e95482dc8d854e73164ec376003
Radixos <radixomen@gmail.com>
2022-09-27 13:13:22 +0100

    * Added presence hub * Added authentication to SignalR * Implemented SignalR on the client side * Added a presence tracker * Added displaying of the online presence * Added a message hub * Added the ability to send messages via the message hub * Added tracking of message groups * Implemented UTC time formating * Added notification for new messages * Optimized presence * Optimized messages

--- API/Extensions/IdentityServiceExtensions.cs
+++ API/Extensions/IdentityServiceExtensions.cs
@@ -33,6 +33,23 @@ namespace API.Extensions
                     ValidateIssuer = false,
                     ValidateAudience = false,
                 };
+
+                //Sending a token as a Query string
+                options.Events = new JwtBearerEvents
+                {
+                    OnMessageReceived = context =>
+                    {
+                        var accessToken = context.Request.Query["access_token"];
+
+                        var path = context.HttpContext.Request.Path;
+                        if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/hubs"))
+                        {
+                            context.Token = accessToken;
+                        }
+
+                        return Task.CompletedTask;
+                    }
+                };
             });
 
             services.AddAuthorization(opt => {
